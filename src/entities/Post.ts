8d13b7779fee866558d688f24c302265f480bae7
Stephen Moore <judo2000@comcast.net>
2022-06-09 20:53:22 -0600

    Create graphql queries to find posts and a single post

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Field, Int, ObjectType } from 'type-graphql';
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: 'date' })
   created_at = new Date();
 
+  @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() })
   updated_at = new Date();
 
+  @Field()
   @Property({ type: 'text' })
   title!: string;
 }
