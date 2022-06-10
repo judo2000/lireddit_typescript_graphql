511343525929a82fe9d43d9e1be4ecf3300331e9
Stephen Moore <judo2000@comcast.net>
2022-06-10 07:25:24 -0600

    Create login function

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,17 +1,45 @@
 import { MyContext } from '../types';
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from 'type-graphql';
 import { User } from '../entities/User';
 import argon2 from 'argon2';
 
 // Doing it a little differently from the way
 // he did PostResolver
 // just to show a different way of doing it
+// A nice thing about using input types is that I
+// can use it for register and login
 @InputType()
 class UsernamePasswordInput {
   @Field()
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
@@ -31,5 +59,38 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx()
+    { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'Invalid login',
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Invalid login',
+          },
+        ],
+      };
+    }
+    return {
+      user,
+    };
   }
 }
