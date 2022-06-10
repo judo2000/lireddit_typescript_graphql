c28ff359c3f125cefae4fc4b587bf28ae1554f7a
Stephen Moore <judo2000@comcast.net>
2022-06-09 21:28:58 -0600

    Create deletePost mutation

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -46,4 +46,14 @@ export class PostResolver {
     }
     return post;
   }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id', () => Int) id: number,
+    @Ctx()
+    { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
+  }
 }
